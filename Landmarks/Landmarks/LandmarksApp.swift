3b0ba91d7adeba4637d9615ac829b490d408f6d3
Jemy <jemy@jemys-mbp.lan>
2022-10-14 13:36:08 -0400

    First commit

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Jemy on 10/14/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
