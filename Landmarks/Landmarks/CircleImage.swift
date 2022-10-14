bd96205c6296115d875b6cf9a58f901a52c69d4b
Jemy <jemy@jemys-mbp.lan>
2022-10-14 14:09:01 -0400

    Added map

--- Landmarks/Landmarks/CircleImage.swift
+++ Landmarks/Landmarks/CircleImage.swift
@@ -12,7 +12,7 @@ struct CircleImage: View {
         Image("turtlerock")
             .clipShape(Circle())
             .overlay {
-                Circle().stroke(.gray, lineWidth: 4)
+                Circle().stroke(.white, lineWidth: 4)
             }
             .shadow(radius: 7)
     }
